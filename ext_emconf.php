ecc8443ef20c6c78e32fc4d08effbbb87df3d0ff
Frans Saris <frans@beech.it>
2015-11-09 10:14:59 +0100

    [TASK] Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.3',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.5.0-7.5.99',
+			'typo3' => '7.5.0-7.6.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
