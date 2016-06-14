59b8c2bb4125ff0727a5863c63b5a82cb91a8627
Frans Saris <frans@beech.it>
2016-06-14 13:34:32 +0200

    [BUGFIX] Register GravatarProvider after DefaultAvatarProvider

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,6 @@ defined('TYPO3_MODE') or die();
 if (TYPO3_MODE === 'BE') {
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['avatarProviders']['gravatarProvider'] = [
         'provider' => \MiniFranske\Gravatar\AvatarProvider\GravatarProvider::class,
-        'after' => ['imageProvider']
+        'after' => ['defaultAvatarProvider']
     ];
 }
