c752639726ade9ec41bad32b51201b58ecc11547
Josef Glatz <josefglatz@gmail.com>
2020-04-07 06:31:46 +0200

    [TASK] Add support TYPO3 10.4 LTS
    Since the extension is actually feature-complete we decided to remove
    support for TYPO3 < 10.
    
    - delete removed ext_emconf.php settings and raise TYPO3 core
      version constraint
    - remove TYPO3_MODE check in ext_localconf.php
    - composer.json: use new spelling for used license
    - composer.json: raise TYPO3 core version constraint
    - composer.json: remove superfluous replace.gravatar option
    - composer.json: add extra.typo3/cms.extension-key option
    - use new ExtensionConfiguration class to retrieve extConf
    - use PHP return type declarations
    - ProxyController: use Request/Response
    - ProxyController: remove bundled guzzle client logic
    - remove bundled guzzle since it is shipped with TYPO3 core
    - GravatarProvider: add missing default setting for useProxy
    - README: add info about versions
    - a11y: remove title attribute in extension icon
    
    Resolves: #11

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['avatarProviders']['gravatarProvider'] = [
-        'provider' => \MiniFranske\Gravatar\AvatarProvider\GravatarProvider::class,
-        'after' => ['defaultAvatarProvider']
-    ];
-}
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['avatarProviders']['gravatarProvider'] = [
+    'provider' => \MiniFranske\Gravatar\AvatarProvider\GravatarProvider::class,
+    'after' => ['defaultAvatarProvider']
+];
