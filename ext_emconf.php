9ff385bf1885b045f5d072a0a7c337e2d762cd40
Frans Saris <franssaris@gmail.com>
2021-09-19 11:30:54 +0200

    [TASK] Raise version to 2.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'author' => 'Frans Saris',
     'author_email' => 'franssaris@gmail.com',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
